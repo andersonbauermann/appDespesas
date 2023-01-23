19528242751fe811e4ff84c7cdb9f6104c2ef1c0
andersonbauermann <anderson.bauermannfeltes@gmail.com>
2023-01-23 20:55:32 -0300

    update app.js
    finalização da aba consulta e melhorias no cadastro de novos itens

--- app.js
+++ app.js
@@ -78,6 +78,12 @@ function cadastrarDespesa() {
         document.getElementById('modal_button').innerHTML = 'Voltar'
         document.getElementById('modal_button').className = 'btn btn-success'
         $('#modalRegistraDespesa').modal('show')
+        dia.value = ''
+        mes.value = ''
+        ano.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
     } else {
         document.getElementById('modal_titulo').innerHTML = 'Erro no cadastro'
         document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
@@ -91,6 +97,28 @@ function cadastrarDespesa() {
 function carregaListaDespesa() {
     let despesas = Array()
     despesas = bd.recuperarTodosRegistros()
+
+    let listaDespesas = document.getElementById('listaDespesas')
+    despesas.forEach(function(d) {
+        let linha = listaDespesas.insertRow()
+
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        switch (d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+        }        
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = `R$ ${d.valor}`
+    })
 }
 
 
