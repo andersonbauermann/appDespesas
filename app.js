65dc77550fdd42e8a23c1369b6798c3863ac09ba
andersonbauermann <anderson.bauermannfeltes@gmail.com>
2023-01-23 07:47:17 -0300

    update app.js
    Modificação no processamento do modal, e início da inclusão da lista de despesas para consulta

--- app.js
+++ app.js
@@ -35,9 +35,23 @@ class Local {
         localStorage.setItem(id, JSON.stringify(d))
         localStorage.setItem('id', id)
     }
+
+    recuperarTodosRegistros() {
+        let despesas = Array()
+        let id = localStorage.getItem('id')
+
+        for (let i = 1; i <= id; i++) {
+            let despesa = JSON.parse(localStorage.getItem(i))
+            despesas.push(despesa)
+            if (despesa === null) {
+                continue
+            }
+        }
+        return despesas
+    }
 }
 
-let novoObjeto = new Local()
+let bd = new Local()
 
 function cadastrarDespesa() {
     let ano = document.getElementById('ano')
@@ -57,11 +71,26 @@ function cadastrarDespesa() {
     )
 
     if(despesa.validarDados()) {
-        novoObjeto.gravar(despesa)
-        $('#sucessoGravacao').modal('show')
+        bd.gravar(despesa)
+        document.getElementById('modal_titulo').innerHTML = 'Cadastro efetuado'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+        document.getElementById('modal_corpo').innerHTML = 'Despesa registrada com sucesso!'
+        document.getElementById('modal_button').innerHTML = 'Voltar'
+        document.getElementById('modal_button').className = 'btn btn-success'
+        $('#modalRegistraDespesa').modal('show')
     } else {
-        $('#erroGravacao').modal('show')
+        document.getElementById('modal_titulo').innerHTML = 'Erro no cadastro'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+        document.getElementById('modal_corpo').innerHTML = 'Não foi possível efetuar o cadastro, existem campos obrigatórios não preenchidos.'
+        document.getElementById('modal_button').innerHTML = 'Voltar e corrigir'
+        document.getElementById('modal_button').className = 'btn btn-danger'
+        $('#modalRegistraDespesa').modal('show')
     }
+}
+
+function carregaListaDespesa() {
+    let despesas = Array()
+    despesas = bd.recuperarTodosRegistros()
 }
 
 
